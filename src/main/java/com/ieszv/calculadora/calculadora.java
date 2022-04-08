dadfe0b8b76d0a745ec442d3203114a1546bb378
victorprofetic <victorprofetic2@gmail.com>
2022-04-08 13:26:11 +0200

    Metodo sumar calculadora

---
+++ src/main/java/com/ieszv/calculadora/calculadora.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ieszv.calculadora;
+
+/**
+ *
+ * @author VICTOR
+ */
+public class calculadora {
+     public static double sumar(double numero1,double numero2) {
+ 
+        return numero1+numero2;
+    }
+}
---
+++ src/test/java/com/ieszv/calculadora/calculadoraTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ieszv.calculadora;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author VICTOR
+ */
+public class calculadoraTest {
+    
+    public calculadoraTest() {
+    }
+
+        /**
+     * Test of sumar method, of class calculadora.
+     */
+    @Test
+    public void testSumar() {
+        System.out.println("sumar");
+        double numero1 = 0.0;
+        double numero2 = 0.0;
+        double expResult = 0.0;
+        double result = calculadora.sumar(numero1, numero2);
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    
+}
